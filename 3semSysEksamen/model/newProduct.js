7670379a868a0e3f94b70d6cca13354d615e62b4
Gustav <70881859+gusajer@users.noreply.github.com>
2021-11-25 14:49:58 +0100

    start af vores tilføj vare funktion

---
+++ 3semSysEksamen/model/newProduct.js
@@ -0,0 +1,7 @@
+function addNewProduct(){
+    var productName = document.getElementById(nameOnProduct).innerHTML;
+    var qty = document.getElementById(quantity).innerHTML;
+    var material = document.getElementById(Material).innerHTML;
+    var klima = document.getElementById().innerHTML;
+    
+}
