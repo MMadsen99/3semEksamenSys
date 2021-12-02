91e658fa9b5379206ea6256a15ea25ed3389b93b
MMadsen99 <math9082@edu.dk>
2021-12-02 10:44:25 +0100

    add category and ccs in admin site

--- 3semSysEksamen/app.js
+++ 3semSysEksamen/app.js
@@ -14,9 +14,9 @@ var adminRouter = require('./routes/admin');
 
 var app = express();
 
-// view engine setup
+// view engine setup'´
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs','html');
 
 app.use(logger('dev'));
 app.use(express.json());
