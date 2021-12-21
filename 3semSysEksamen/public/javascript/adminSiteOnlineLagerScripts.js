7b206717b32198aa89eafadb374cab11213753e7
MMadsen99 <math9082@edu.dk>
2021-12-21 11:25:37 +0100

    Cleaned comments

--- 3semSysEksamen/public/javascript/adminSiteOnlineLagerScripts.js
+++ 3semSysEksamen/public/javascript/adminSiteOnlineLagerScripts.js
@@ -1,44 +1,35 @@
 const selector = document.getElementById('ID');
 
-
-
 async function setSpinnerID () {
 
     let items = await fetch('http://localhost:3000/items');
     let jsonItems = await items.json();
  
-
-
 for (let i = 0; i < jsonItems.allItemsInDb.length; i++) {
     
     let opt = document.createElement('option');
     opt.value = jsonItems.allItemsInDb[i]._id;
     opt.innerHTML = jsonItems.allItemsInDb[i].title;
     selector.appendChild(opt);
-    
-    
 }
-
 }
 
 async function deleteItem() {
     let id = selector.value;
     let items = await fetch(`http://localhost:3000/deleteItem/${id}`);
-   // let url = new URL('http://localhost:3000/adminSiteOnlineLager.html');
-    //document.location = url;
     location.reload(true);
 }
 setSpinnerID();
 
  async function tableFromJson() {
-
+    //json data. 
     let items = await fetch('http://localhost:3000/items');
     let jsonItems = await items.json();
-    // the json data. (you can change the values for output.)
+   
     
 
     // Extract value from table header. 
-    // ('Book ID', 'Book Name', 'Category' and 'Price')
+    // ('title', 'material', 'Category' and 'Price' osv)
     var col = [];
     for (var i = 0; i < jsonItems.allItemsInDb.length; i++) {
         for (var key in jsonItems.allItemsInDb[i]) {
@@ -77,58 +68,3 @@ setSpinnerID();
     divShowData.appendChild(table);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-async function tableCreate() {
-
-    let items = await fetch('http://localhost:3000/items');
-    let jsonItems = await items.json();
- 
-    const body = document.body,
-          tbl = document.createElement('table');
-    tbl.style.width = '100px';
-    tbl.style.border = '1px solid black';
-                         
-    for (let i = 0; i <= jsonItems.allItemsInDb.length; i++) {
-      const tr = tbl.insertRow();
-                       
-      for (let j = 0; j < Object.keys(jsonItems).length; j++) {
-        if (i === 2 && j === 1) {
-          break;
-        } else {
-          const td = tr.insertCell();
-          td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
-          td.style.border = '1px solid black';
-          if (i === 1 && j === 1) {
-            td.setAttribute('rowSpan', '2');
-          }
-        }
-      }
-    }
-    body.appendChild(tbl);
-  }
-  
-  tableCreate();
-*/
