19563d12109122d9813fb5374bb1a38815896147
KRL1991 <kimx4706@edu.easj.dk>
2021-12-28 10:18:05 +0100

    Refactored and removed som unecessary code

--- 3semSysEksamen/public/javascript/productInformation.js
+++ 3semSysEksamen/public/javascript/productInformation.js
@@ -2,25 +2,6 @@
  const pris =document.getElementById("pris");
  const information = document.getElementById("information");
  const potteName = document.getElementById("name");
- //const button1 = document.getElementById("button1");
-
-
-  function test(){
-
-  /*
-  let items = await fetch('http://localhost:3000/items');
-    let jsonItems = await items.json();
-
-    
-
-    if(button1.onclick == true){
-    potteName.innerHTML = jsonItems.allItemsInDb[0].title;
-    information.innerHTML = jsonItems.allItemsInDb[0].description;
-    pris.innerHTML = "Det virker";
-    } */
- }
-
- test();
 
  const buttonPrice = sessionStorage.getItem("price") + " kr.";
  document.getElementById("productPrice").innerHTML = buttonPrice; 
