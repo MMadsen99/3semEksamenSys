54e7628d1f8142949be4daf9266d83f88b65a04e
Andreas Ravn <ravnen707@gmail.com>
2021-12-28 09:41:31 +0100

    Taking info from one to another cata => ProInfo

--- 3semSysEksamen/public/javascript/productInformation.js
+++ 3semSysEksamen/public/javascript/productInformation.js
@@ -21,3 +21,12 @@
  }
 
  test();
+
+ const buttonPrice = sessionStorage.getItem("price") + " kr.";
+ document.getElementById("productPrice").innerHTML = buttonPrice; 
+
+ const buttonName = sessionStorage.getItem("name");
+ document.getElementById("productName").innerHTML = buttonName;  
+
+ const buttonDescription = sessionStorage.getItem("description");
+ document.getElementById("productDescription").innerHTML = buttonDescription;  
